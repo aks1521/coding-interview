eb9466eca7498da6d702188cf5a32b962da10732
Amplyp <amply143@gmail.com>
2020-01-22 00:43:38 +0530

    Added Code to sum all prime numbers up to a given number

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,5 +1,20 @@
 function sumAllPrimes(num: number): number {
+  let isPrimeNumber;
+  let result=0;
+  for(let intermediateNumber=2; intermediateNumber<=num; intermediateNumber++){
+    isPrimeNumber=true;
+    for(let i=2; i<= Math.floor(Math.sqrt(intermediateNumber)); i++){
+        if(intermediateNumber%i==0){
+            isPrimeNumber=false;
+            break;
+        }
+    }
 
+    if(isPrimeNumber){
+        result+=intermediateNumber;
+    }
+  } 
+  return result;
 }
 
 console.log(sumAllPrimes(10));
