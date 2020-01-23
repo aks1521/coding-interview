39eed95b31825af469c8169184f4d8c69d2fe63d
Amplyp <amply143@gmail.com>
2020-01-23 10:16:29 +0530

    Added code for sortByHeight

--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,5 +1,15 @@
-function sortByHeight(a: number[]): number[] {
+function sortByHeight(inputHeigts: number[]): number[] {
+    let indexTracker:number=0;
+    let filteredHeights: number[]=inputHeigts.filter(height=> height!=-1);
+    filteredHeights.sort();
 
+    for(let i:number=0; i<inputHeigts.length; i++){
+        if(inputHeigts[i]!=-1){
+            inputHeigts[i]=filteredHeights[indexTracker];
+            indexTracker++;
+        }
+    }
+    return inputHeigts;
 }
 
 console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
