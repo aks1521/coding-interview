342abb9bedfa9271e769813079ece9c7e1e01ab5
Amplyp <amply143@gmail.com>
2020-01-29 10:29:19 +0530

    Added code for validtime algo

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,5 +1,15 @@
 function validTime(time: string): boolean {
+   let timeArr=time.split(':');
+   let hours: number =parseInt(timeArr[0]);
+   let min: number =parseInt(timeArr[1]);
+   if(hours==24 && min==0){
+       return true;
+   }
+   else if(hours<24 && min<60){
+       return true;
+   }
 
+   return false;
 }
 
 console.log(validTime('13:58'));
