5aaffc4148e3bbd92a713bb0a67ee8fd75da8cd1
Amit_k_p <amit.p@icreon.com>
2020-01-30 19:11:40 +0530

    Added code for switchLights algo

--- switchLights/switchLights.ts
+++ switchLights/switchLights.ts
@@ -1,5 +1,26 @@
 function switchLights(a: number[]): number[] {
+   let candles : number =a.length;
+   let output: number[]=[];
+   let appliedIterationOnCandle: number;
+   let candleState: number;
+   
+   for(let i:number=0; i<candles; i++){
+        appliedIterationOnCandle=candles-i;
+        candleState=getToggle(a[i], appliedIterationOnCandle);
+        output.push(candleState); 
+   }
+   return output;
+}
 
+function getToggle(candleState: number, appliedIterationOnCandle:number):number{  
+    let result:number;
+    if(appliedIterationOnCandle%2!=0){
+       result=candleState==1?0:1;
+   }
+   else{
+       result=candleState;
+   }
+   return result;
 }
 
 
