b104b9ec812a49ed988d567e51297dc1a8275497
Amplyp <amply143@gmail.com>
2020-01-31 10:06:04 +0530

    Added code for uniqueDigitsProducts algo

--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,4 +1,31 @@
 function uniqueDigitProducts(a: number[]): number {
+    let uniqueDigitProducts:number[]=[];
+    let digitsArray:string[]=[];
+    let productOfAnElementOfArray:number;
+
+    a.forEach(num=>{
+        digitsArray=num.toString().split("");
+        productOfAnElementOfArray=getProductOfAnArrayElements(digitsArray);
+        if(uniqueDigitProducts.indexOf(productOfAnElementOfArray)==-1){
+            uniqueDigitProducts.push(productOfAnElementOfArray);
+        };
+    });
+   
+    return uniqueDigitProducts.length;
+}
+
+function getProductOfAnArrayElements(num: string[]):number{
+  let product: number=1;
+  num.forEach(element => {
+    product*=parseInt(element);
+  });
+  return product;
 }
 
 console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
+
+console.log(uniqueDigitProducts([2, 8, 5, 433, 222, 23]));
+
+console.log(uniqueDigitProducts([2, 6, 8, 42, 66, 16]));
+
+console.log(uniqueDigitProducts([2, 8, 865, 42, 12, 95]));
